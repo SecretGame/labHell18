cab7d7311ea638720358a9728f5d9978d017c890
GGgun2013 <gggun2013@gmail.com>
2021-02-23 16:11:18 +0700

    lab18_2Clear

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -5,8 +5,24 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect *R1,Rect *R2){
+	double x2,y2,a2,b2;
+	double Overlape;
+	double sumh,sumw;
+	x2 = R1->x + R1->w;
+	y2 = R1->y - R1->h;
+	a2 = R2->x + R2->w;
+	b2 = R2->y - R2->h;
+	sumh = min(x2,a2) - max(R1->x,R2->x);
+	sumw = min(R1->y,R2->y) - max(y2,b2);
+	Overlape = sumh*sumw;
+    if(Overlape > 0)
+		return Overlape;
+	else return 0;
+}
 
-
-
+int main(){
+	Rect R1 = {1,1,5,5};
+	Rect R2 = {2,2,5,5};	
+	cout << overlap(&R1,&R2);
 }
